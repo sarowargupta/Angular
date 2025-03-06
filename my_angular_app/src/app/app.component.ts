dc4280c6551dd547056495caa8adfbee9611a316
sarowargupta <sarowargupta2001@gmail.com>
2025-03-06 15:39:52 +0530

    [Sarowar Gupta]add. adding link to logo.

--- my_angular_app/src/app/app.component.ts
+++ my_angular_app/src/app/app.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = "Hello from Bridegelabz";  
-  logoPath: string = "assets/BL_logo_square_jpg.jpg"; 
+  title = 'HelloWorldApp';
+  logoPath: string ='assets/BL_logo_square_jpg.jpg';
+  url = "https://www.bridgelabz.com/"
+
+  ngOnInit(): void{
+    this.title = "Hello from BridgeLabz."
+  }
+
+  onClick($event : MouseEvent){
+    console.log("Save Button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
 }
